9dd770365c58d5470f067fac110e33b2e4ef0b66
Tom Gurvich <tomdrc4@gmail.com>
2019-06-07 19:53:02 +0300

    Added a not found html file. Splitted the handle_get_request function into more functions for cleaner code. Added a match statement to check if there's an error when opening the file which means the file doesn't exist and we will send the not found file instead.

--- src/HttpWebServer.rs
+++ src/HttpWebServer.rs
@@ -67,7 +67,7 @@ impl HttpWebServer
     /// Will handle the request if it is an HTTP GET request. Private function to help the code be cleaner
     /// 
     /// # Arguments
-    /// * `msg_from_client` - The message the client has sent us. This will usually be an HTTP GET request that we will parse to get the wanted data.
+    /// * `msg_from_client` - A string that holds the message the client has sent.
     /// * `stream` - A mutable refrence of a TcpStream. So we will be able to send the client the message and after we're done with the function we will return the TcpStream we borrowed.
     /// 
     /// # Example
@@ -85,28 +85,22 @@ impl HttpWebServer
     fn handle_get_request(&self, msg_from_client: String, stream: &mut TcpStream)
     {
         let file_name = self.get_file_name(msg_from_client);
+        let full_response = self.get_response_message_u8(file_name);
 
-        // After I read the file data I make a response with the wanted headers. I turn into a byte vector to append it the file data which is a byte vector as well.
-        let mut file_data = fs::read(format!("htmlFiles\\{}", file_name)).unwrap();
-        let mut response = "HTTP/1.1 200 OK\r\n\r\n".to_string().into_bytes();
-
-        response.append(&mut file_data);
-        
-        stream.write(&response[..]).unwrap();
+        stream.write(&full_response[..]).unwrap();
         stream.flush().unwrap();
     }
 
     /// Returns the file name from the msg to the client.
     /// 
     /// # Arguments
-    /// * `msg_from_client` - This is the message the client sent. As this is a helper function for the `handle_get_request` this will usually be an HTTP GET request message
+    /// * `msg_from_client` - A string that holds the message a client has sent
     fn get_file_name(&self, msg_from_client: String) -> String
     {
         let a: Vec<&str> = msg_from_client.split(" ").collect();
 
         // get fileName
         let file_name = a[1].to_string();
-        println!("{}", file_name);
 
         if file_name == "/"
         {
@@ -114,5 +108,37 @@ impl HttpWebServer
         }
         
         file_name.replace("/", "")
+    }
+
+    /// Returns the full response message with the headers and the file data. The data will be returned in a Byte vector format.
+    /// 
+    /// # Arguments 
+    /// * `file_name` - A string that holds the file name
+    /// 
+    /// # Example
+    /// ```
+    /// let full_response = self.get_response_message_u8(file_name);
+    /// // send the response to the client, as the `write` function only passes a byte array refrence we make the array into that.
+    /// stream.write(&full_response[..]).unwrap();
+    /// stream.flush().unwrap();
+    /// ```
+    fn get_response_message_u8(&self, file_name: String) -> Vec<u8>
+    {
+        // Set 200 Ok as deafult if there is an error (usually a not found error) change response to error headers.
+        let mut response = "HTTP/1.1 200 OK\r\n\r\n".to_string().into_bytes();
+        let mut file_data = match fs::read(format!("htmlFiles\\{}", file_name))
+        {
+            Ok(file) => file,
+            Err(_) =>
+            {
+                response = "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string().into_bytes();
+                fs::read("htmlFiles\\404_not_found.html").unwrap()
+            }
+        };
+
+        // Append data to the headers
+        response.append(&mut file_data);
+
+        response
     }
 }
